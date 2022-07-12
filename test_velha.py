a71fad6eb34d0a7f713f92a2f094e234089d2ed6
Gus_SLima <211038235@aluno.unb.br>
2022-07-12 17:36:57 -0300

    teste 2

---
+++ test_velha.py
@@ -0,0 +1,5 @@
+from velha import Velha
+assert Velha([[2,2,2],[2,2,2],[1,1,1]])==-2
+assert Velha([[1,1,1],[2,2,2],[1,1,1]])==-2
+assert Velha([[1,1,1],[1,2,1],[2,2,2]])is None
+assert Velha([[1,0,1],[2,2,0],[2,1,0]])is None
