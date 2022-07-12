02b859269e5e9d5c181ee8bbaf03ff1552caa9a6
Gus_SLima <211038235@aluno.unb.br>
2022-07-12 18:06:39 -0300

    teste 1

--- velha.py
+++ velha.py
@@ -12,14 +12,3 @@ def Velha(matriz):
                 v+=1
     if x-c>1 or x-c<0:
         return -2
-    for i in range(3):
-        if matriz[i][0]==matriz[i][1] and matriz[i][1]==matriz[i][2]:
-            if matriz[i][0]==1:
-                return 1
-            else:
-                return 2
-        elif matriz[0][i]==matriz[1][i] and matriz[1][i]==matriz[2][1]:
-            if matriz[i][0]==1:
-                return 1
-            else:
-                return 2
